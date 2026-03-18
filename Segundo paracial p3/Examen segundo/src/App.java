208aa0e8f825d8f55aef0edc04ae30e89e03eb7a
Ricky045 <ricky_vargas@ucne.edu.do>
2026-03-18 17:51:20 -0400

    Add files via upload

---
+++ Segundo paracial p3/Examen segundo/src/App.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage stage){
+        stage.setTitle("EL mejor");
+        stage.show();
+    }
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+}
